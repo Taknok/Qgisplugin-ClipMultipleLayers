5970b8a76ec22f033c6b9f8165a1973ec417e790
zsiki <siki1958@gmail.com>
2020-03-08 17:57:20 +0100

    Messages translated to Hungarian

---
+++ i18n/hu.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Jó reggelt!</translation>
+    </message>
+</context>
+<context>
+    <name>ClipMultipleLayers</name>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="190"/>
+        <source>&amp;Clip Multiple Layers</source>
+        <translation>Rétegek vágása</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="180"/>
+        <source>Clip all displayed layers</source>
+        <translation>Minden látható réteg vágása</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="207"/>
+        <source>Select output folder </source>
+        <translation>Válassz cél mappát</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="256"/>
+        <source>Warning</source>
+        <translation>Figyelmeztetés</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="256"/>
+        <source>Neither vector nor raster layers selected for clipping. Nothing to do. </source>
+        <translation>Sem a vektor, sem a raszter rétegeket nem választotta a vágáshoz. Nincs mit csinálni.</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="280"/>
+        <source>Clipping...</source>
+        <translation>Vágás...</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="338"/>
+        <source>Error</source>
+        <translation>Hiba</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="338"/>
+        <source>Could not load </source>
+        <translation>Nem lehet betölteni</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="26"/>
+        <source>ClipMultipleLayers</source>
+        <translation>Rétegek vágása</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="32"/>
+        <source>Suggestions ? Feedback ? -&gt; pg.developper.fr@gmail.com</source>
+        <translation>Javaslatok? Visszajelzések? -&gt; pg.developper.fr@gmail.com</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="44"/>
+        <source>Load clipped layers</source>
+        <translation>Vágott rétegek betöltése</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="84"/>
+        <source>Selection :</source>
+        <translation>Vágó réteg:</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="111"/>
+        <source>Clip vector layers</source>
+        <translation>Vektor rétegek vágása</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="121"/>
+        <source>Clip raster layers</source>
+        <translation>Raszter rétegek vágása</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="135"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please consider to rate the &lt;a href=&quot;https://plugins.qgis.org/plugins/ClipMultipleLayers/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;plugin&lt;/span&gt;&lt;/a&gt; or &lt;a href=&quot;https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;amp;hosted_button_id=4DC3A78C398HW&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;support me&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Kérjük szavazz a &lt;a href=&quot;https://plugins.qgis.org/plugins/ClipMultipleLayers/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;modulra&lt;/span&gt;&lt;/a&gt; vagy &lt;a href=&quot;https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;amp;hosted_button_id=4DC3A78C398HW&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;támogass&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="163"/>
+        <source>Clip all displayed layers with the selection layer :</source>
+        <translation>Az összes látható réteg vágása a vágó réteggel</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="172"/>
+        <source>Output :</source>
+        <translation>Cél mappa:</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="186"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="54"/>
+        <source>Save vector styles (qml)</source>
+        <translation>Vektor stílusok mentése</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="237"/>
+        <source>No polygon layer in actual project</source>
+        <translation>Nincs felület réter az aktuális projektben</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers.py" line="251"/>
+        <source>Please select target folder</source>
+        <translation>Válassz cél mappát</translation>
+    </message>
+    <message>
+        <location filename="../clip_multiple_layers_dialog_base.ui" line="63"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;More instructions on the &lt;a href=&quot;https://github.com/Taknok/Qgisplugin-ClipMultipleLayers/wiki&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;home page&lt;/span&gt;&lt;/a&gt;. Report issues &lt;a href=&quot;https://github.com/Taknok/Qgisplugin-ClipMultipleLayers/issues&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;here&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;⚠️ Rasters clipping may freez Qgis on small processors, &lt;br/&gt;Qgis will unfreez when the cut is finished.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;További információk a &lt;a href=&quot;https://github.com/Taknok/Qgisplugin-ClipMultipleLayers/wiki&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;holapon&lt;/span&gt;&lt;/a&gt;. Hibák jelzése &lt;a href=&quot;https://github.com/Taknok/Qgisplugin-ClipMultipleLayers/issues&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;itt&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;⚠️ Lassú processzoron raszterek vágása blokkolhatja a QGIS-t, &lt;br/&gt;a vágás befejezése után a QGIS tovább dolgozik.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+</TS>
