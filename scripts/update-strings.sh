4c964d7d602c4ede132ea6f09351cf0db035f8ec
zsiki <siki1958@gmail.com>
2020-03-08 17:59:29 +0100

    new options added to dialog

--- scripts/update-strings.sh
+++ scripts/update-strings.sh
@@ -40,7 +40,7 @@ done
 if [ ${UPDATE} == true ]
 # retrieve all python files
 then
-  print ${PYTHON_FILES}
+  echo ${PYTHON_FILES}
   # update .ts
   echo "Please provide translations by editing the translation files below:"
   for LOCALE in ${LOCALES}
@@ -48,7 +48,7 @@ then
     echo "i18n/"${LOCALE}".ts"
     # Note we don't use pylupdate with qt .pro file approach as it is flakey
     # about what is made available.
-    pylupdate4 -noobsolete ${PYTHON_FILES} -ts i18n/${LOCALE}.ts
+    pylupdate5 -noobsolete ${PYTHON_FILES} -ts i18n/${LOCALE}.ts
   done
 else
   echo "No need to edit any translation files (.ts) because no python files"
